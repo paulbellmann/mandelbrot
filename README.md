0f8484521fa073759853dca4396935b84fb6904c
paulbellmann <paul.bellmann@yahoo.de>
2018-05-30 10:58:49 +0200

    + initial

---
+++ README.md
@@ -0,0 +1,28 @@
+### Mandelbrot Set
+
+---
+
+This is a simple Mandelbrot Python backend. It can calculate any given area and returns it values
+in a array.
+
+## How to run the development server on your system: 
+
+1. `$ pip install -r requirements.txt`
+2. `$ export FLASK_APP=server.py`
+3. `$ flask run`
+
+## How to test the server:
+
+```
+$ curl --header "Content-Type: application/json" \
+       --request POST \
+       --data '{"realFrom":-2, "realTo":1, "imgFrom": -1, "imgTo":1, "interval": 0.01, "max_iter": 100}' \
+       http://localhost:5000
+```
+
+## How to test the Mandelbrot Class:
+
+1. `$ python test_plot.py`
+2. watch the plot
+3. change constructor params in `test_plot.py`
+4. repeat steps 1+2
---
+++ mandelbrot.py
@@ -0,0 +1,48 @@
+import numpy
+import matplotlib.pyplot as plt
+
+class Mandelbrot(object):
+    """Creates a Mandelbrot object with various constructor parameters"""
+
+    def __init__(self, realFrom, realTo, imgFrom, imgTo, interval, max_iter):
+        self.realFrom, self.realTo = realFrom, realTo
+        self.imgFrom, self.imgTo = imgFrom, imgTo
+        self.interval, self.max_iter = interval, max_iter
+
+    def mandelbrot(self, Re, Im, max_iter):
+        """calculates how many iterations it takes to get |Zn| = 2"""
+        c = complex(Re, Im)
+        z = 0.0j
+
+        for i in xrange(max_iter):
+            z = z*z + c
+            # |Zn| = 2
+            if (z.real*z.real + z.imag*z.imag) >= 4:
+                return i
+
+        return max_iter
+
+    def do_the_loop(self):
+        """loops through realFrom -> realTo and also imgFrom -> imgTo with the given interval step 
+        and creates array with the values from mandelbrot()"""
+        columns = len(numpy.arange(self.imgFrom, self.imgTo, self.interval))
+        rows = len(numpy.arange(self.realFrom, self.realTo, self.interval))
+
+        # creates array with x / y zeros
+        self.result = numpy.zeros([rows, columns])
+
+        # real axis
+        for row_index, Re in enumerate(numpy.arange(self.realFrom, self.realTo, self.interval)):
+            # imag axis
+            for column_index, Im in enumerate(numpy.arange(self.imgFrom, self.imgTo, self.interval)):
+                self.result[row_index, column_index] = self.mandelbrot(Re, Im, self.max_iter)
+
+        return self.result
+
+    def show_image(self):
+        """shows a plot for local testing"""
+        plt.figure(dpi=100)
+        plt.imshow(self.result.T, cmap='hot', interpolation='bilinear', extent=[-2, 1, -1, 1])
+        plt.xlabel('Re')
+        plt.ylabel('Im')
+        plt.show()
